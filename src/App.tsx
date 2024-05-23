c3600b3a46d02524e76aacd8e58515cf88fc80bd
ermanzohre <ermanzohre@gmail.com>
2024-05-23 23:12:46 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetter, setGuessedLetter] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+      
+        Lose Win
+      </div>
+    </div>
+  );
+}
+
+export default App;
